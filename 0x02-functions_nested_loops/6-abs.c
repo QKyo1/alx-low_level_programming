fc28255cf414acbf73f953e464d90b39972a7003
QKyo1 <shika.kiyama@gmail.com>
2023-09-12 17:39:51 -0700

    6.23

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,16 +6,11 @@
 *
 * Return: Always 0.
 */
-int main(void)
+int _abs(int n)
 {
-int r;
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
 }
