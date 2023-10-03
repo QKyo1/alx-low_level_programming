dab20b3e3c9a6545577d7b6b06ee16c09899c5c6
QKyo1 <shika.kiyama@gmail.com>
2023-10-02 19:41:17 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - wllh
+* @argc: ma
+* @argv: b8dr
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+(void) argv; /*Ignore argv*/
+printf("%d\n", argc - 1);
+return (0);
+}
