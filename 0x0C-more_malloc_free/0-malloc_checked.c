73e08b19da1c0cab5376ae95d6f9ca0006757abe
QKyo1 <shika.kiyama@gmail.com>
2023-10-08 16:50:24 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* *malloc_checked - the task name
+* @b - size of the block
+* return - 98
+*/
+
+void *malloc_checked(unsigned int b)
+{
+int *x;
+x = malloc(b);
+if (x == NULL)
+{
+return (98);
+}
+return (x);
+}
