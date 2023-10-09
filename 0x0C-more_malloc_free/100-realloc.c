d694423b4d0f31510fac4e1faa0a9dbd6d56824c
QKyo1 <shika.kiyama@gmail.com>
2023-10-08 19:26:54 -0700

    100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* *_realloc - task name
+* @ptr: pointer 
+* @old_size: sizold
+* @new_size: newsize 
+* Return: poter
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *ptr1;
+char *old_ptr;
+unsigned int x;
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr)
+{
+free(ptr);
+return (NULL);
+}
+if (!ptr)
+return (malloc(new_size));
+ptr1 = malloc(new_size);
+if (!ptr1)
+return (NULL);
+old_ptr = ptr;
+if (new_size < old_size)
+{
+for (x = 0; x < new_size; x++)
+ptr1[x] = old_ptr[x];
+}
+if (new_size > old_size)
+{
+for (x = 0; x < old_size; x++)
+ptr1[x] = old_ptr[x];
+}
+free(ptr);
+return (ptr1);
+}
