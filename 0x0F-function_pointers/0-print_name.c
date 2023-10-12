134b1d5f4c0f8e094fec302f75584c5ced09e391
QKyo1 <shika.kiyama@gmail.com>
2023-10-11 19:25:59 -0700

    0.232

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,16 @@
 #include <function_pointers.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
 * print_name - task name
 * @name: bla bla
 * @f: n
-* Return: 0
 */
 
 void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
-return;
+exit(1);
 f(name);
-};
+}
