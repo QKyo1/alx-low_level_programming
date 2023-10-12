f4af1cc12ed717e96cdb4e804003669f8b4e1ba5
QKyo1 <shika.kiyama@gmail.com>
2023-10-11 17:35:02 -0700

    0

---
+++ 0x0F-function-pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include<function_pointers.h>
+
+/**
+* print_name - task name
+* @name - bla bla
+* @f - ok
+* return - 0
+*/
+
+void print_name(char *name, void (*f)(char *));
+{
+if (name && f)
+f(name);
+}
