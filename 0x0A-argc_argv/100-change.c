1c894e816b551b535ed13bc8d5513e5f37150682
qkyo1 <awabelnus@gmail.com>
2023-10-23 16:15:00 -0700

    added

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* change - a recursive * operation
+* @x: int
+* Return: 0
+*/
+
+int change(int x)
+{
+if (x / 25 > 0)
+return (x / 25 + change(x % 25));
+else if (x / 10 > 0)
+return (x / 10 + change(x % 10));
+else if (x / 5 > 0)
+return (x / 5 + change(x % 5));
+else if (x / 2 > 0)
+return (x / 2 + change(x % 2));
+else if (x / 1 > 0)
+return (x / 1);
+return (0);
+}
+
+/**
+* main - calculates the amount of change
+* @argc: numbers
+* @argv: arguments
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+int x, y;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+i = atoi(argv[1]);
+if (x <= 0)
+{
+printf("0\n");
+return (0);
+}
+y = change(x);
+printf("%d\n", y);
+return (0);
+}
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,64 +1,24 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* _atoi - koo7
-* @s: koo7
+* main - entry point
+* @argc: argum
+* @argv: argu
 * Return: 0
 */
 
-int _atoi(char *s)
+int main(int argc, char **argv)
 {
-int x, y, z, len, w, digit;
-x = 0;
-y = 0;
-z = 0;
-len = 0;
-w = 0;
-digit = 0;
-while (s[len] != '\0')
-len++;
-while (x < len && w == 0)
-{
-if (s[x] == '-')
-++d;
-if (s[x] >= '0' && s[x] <= '9')
-{
-digit = s[x] - '0';
-if (y % 2)
-digit = -digit;
-z = z * 10 + digit;
-w = 1;
-if (s[x + 1] < '0' || s[x + 1] > '9')
-break;
-w = 0;
-}
-x++;
-}
-if (w == 0)
-return (0);
-return (z);
-}
-
-/**
-* main - kfaya
-* @argc: ok
-* @argv: zab6
-* Return: 0 (Success), 1 (Error)
-*/
-
-int main(int argc, char *argv[])
-{
-int r, s, t;
-if (argc < 3 || argc > 3)
+int x, y;
+if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
-s = _atoi(argv[1]);
-t = _atoi(argv[2]);
-r = s *t;
-printf("%d\n", r);
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+printf("%d\n", x *y);
 return (0);
-}
 }
