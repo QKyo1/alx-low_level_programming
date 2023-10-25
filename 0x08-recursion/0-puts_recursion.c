b0896a54cbbda8d09bb573b3c219b74a3511fb9f
qkyo1 <engawab8@gmail.com>
2023-10-25 12:04:16 -0700

    0&1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,7 +4,7 @@
 * @s: input
 * Return: 0
 */
-void _puts_recursion(char*s)
+void _puts_recursion(char *s)
 {
 if (s)
 {
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "main.h"
+
+/**
+* _print_rev_recursion - Prints in reverse
+* @s: sring
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
