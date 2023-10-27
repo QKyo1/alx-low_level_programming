88495ae9dc10f4542cf26deaac6b241c0791cde7
qkyo1 <engawab8@gmail.com>
2023-10-27 04:29:22 -0700

    0&1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program to write the name
+ * @argc: arguments
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv)
+{
+printf("%s\n" , *argv);
+return (0);
+}
