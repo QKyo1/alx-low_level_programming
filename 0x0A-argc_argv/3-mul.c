0da6225b32a50a43a9dff142049f824d2a35b54d
qkyo1 <engawab8@gmail.com>
2023-10-27 09:32:34 -0700

    ali]

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * main - multi numbers
- * @argc: argum
+ * main - task to print
+ * @argc: arguments
  * @argv: array
- * Return: 0
+ * Return: 0 - 1
  */
 
 int main(int argc, char **argv)
 {
-int result, x, y;
+int x, y;
 if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
-x = _atoi(argv[1]);
-y = _atoi(argv[2]);
-result = x *y;
-printf("%d\n", result);
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+printf("%d\n", x *y);
 return (0);
 }
