5233c8e78def8c0af90346b74e05f47948a3d70e
qkyo1 <engawab8@gmail.com>
2023-11-02 11:51:22 -0700

    please

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,35 +12,29 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *a;
-char *b, *c;
-unsigned int x = 0;
-unsigned int y;
-if (ptr == NULL)
-{
-a = malloc(new_size);
-return (a);
-}
+char *a;
+char *b;
+unsigned int x;
 if (old_size == new_size)
 return (ptr);
-if (new_size == 0)
+if (new_size == 0 && ptr)
 {
 free(ptr);
 return (NULL);
 }
-a = malloc(new_size);
-if (a == NULL)
+if (!ptr)
 return (NULL);
-if (old_size > new_size)
-y = old_size;
-else
-y = new_size;
-b = a;
-c = ptr;
-while (x < y)
+b = ptr;
+if (new_size < old_size)
 {
-b[x] = c[x];
-x++;
-return (a);
+for (x = 0; x < new_size; x++)
+a[x] = b[x];
 }
+if (new_size > old_size)
+{
+for (x = 0; x < old_size; x++)
+a[x] = b[x];
+}
+free(ptr);
+return (a);
 }
