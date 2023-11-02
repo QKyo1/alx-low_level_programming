751404ffa928d5a3d03cf31617e044290654c540
qkyo1 <engawab8@gmail.com>
2023-11-02 09:10:13 -0700

    09

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,9 +15,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *g;
 unsigned int x = 0, y = 0, len1 = 0, len2 = 0;
 while (s1 && s1[len1])
-	len1++;
+len1++;
 while (len2 && s2[len2])
-	len2++;
+len2++;
 if (n < len2)
 g = malloc(sizeof(char) * (len1 + n + 1));
 else
