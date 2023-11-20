b2e857a8102f4e141e2f10bcd28bed94d818bad7
qkyo1 <engawab8@gmail.com>
2023-11-20 22:47:13 +0400

    2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* add_nodeint - adds a nod3 to the list
+* @head: point3r as a head of the nod3
+* @n: pointer
+* Return: num,bree
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new;
+new = malloc(sizeof(listint_t));
+if (!x)
+return (NULL);
+x->n = n;
+x->next = *head;
+*head = x;
+return (x);
+}
