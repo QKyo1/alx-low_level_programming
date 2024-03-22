fae79123733602e1afb9f12456c8f09f8283135a
qkyo1 <engawab8@gmail.com>
2024-03-23 02:07:05 +0400

    5555555

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,18 +3,20 @@
 """
 island_perimeter module
 
-This module defines a function to calcula
-An island is considered a group of
-The perimeter is the total number of
+This module defines a function to calculate
+
+An island is considered a group of connected 1s
+The perimeter is the total number of sides bordering water.
 """
 
 
 def island_perimeter(grid):
+
     """
   Calculates the perimeter of the island in a grid.
 
   Args:
-      grid: A list of lists representing the grid
+      grid: A list where 0 represents water and 1
 
   Returns:
       The perimeter of the island in the grid.
@@ -27,9 +29,9 @@ perimeter = 0
 for row in range(rows):
     for col in range(cols):
         if grid[row][col] == 1:
-            # Check all four neighbors for water (0) and add1 to if found
+            # Check all four neighbors for water (0) and add 1
             perimeter += (1 if row == 0 or grid[row - 1][col] == 0 else 0)
-        perimeter += (1 if col == 0 or grid[row][col - 1] == 0 else 0)
+            perimeter += (1 if col == 0 or grid[row][col - 1] == 0 else 0)
         perimeter += (1 if row == rows - 1 or grid[row + 1][col] == 0 else 0)
         perimeter += (1 if col == cols - 1 or grid[row][col + 1] == 0 else 0)
 
