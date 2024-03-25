566b4ad7f95ab7ab34d2752de5c101fe68f66fd0
qkyo1 <engawab8@gmail.com>
2024-03-25 04:34:34 +0400

    fix 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,28 +11,25 @@ The perimeter is the total number of sides bordering water.
 
 
 def island_perimeter(grid):
-
     """
   Calculates the perimeter of the island in a grid.
 
   Args:
-      grid: A list where 0 represents water and 1
+      grid: A list of lists representing the grid
 
   Returns:
       The perimeter of the island in the grid.
-  """
+    """
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
 
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                # Check all four neighborsforwater(0)andadd1toperimeter ifdound
+                perimeter += (1 if row == 0 or grid[row - 1][col] == 0 else 0)
+                perimeter += (1 if col == 0 or grid[row][col - 1] == 0 else 0)
+                perimeter += (1 if row == rows - 1 or grid[row + 1][col] == 0 else 0)
+                perimeter += (1 if col == cols - 1 or grid[row][col + 1] == 0 else 0)
 
-rows, cols = len(grid), len(grid[0])
-perimeter = 0
-
-for row in range(rows):
-    for col in range(cols):
-        if grid[row][col] == 1:
-            # Check all four neighbors for water (0) and add 1
-            perimeter += (1 if row == 0 or grid[row - 1][col] == 0 else 0)
-            perimeter += (1 if col == 0 or grid[row][col - 1] == 0 else 0)
-        perimeter += (1 if row == rows - 1 or grid[row + 1][col] == 0 else 0)
-        perimeter += (1 if col == cols - 1 or grid[row][col + 1] == 0 else 0)
-
-return perimeter
+                return perimeter
